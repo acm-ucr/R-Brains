d805910da82bc360cc8ed1b83a8313d731b9e068
4skate <tyler06.t.e@gmail.com>
2025-05-07 03:48:32 -0700

    did basic setup, still working on the issue

--- src/app/about/AboutUs.tsx
+++ src/app/about/AboutUs.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+// import React from "react";
 
-const AboutUs = () => {
-    <div>
-        
-    </div>
-};
+// const AboutUs = () => {
+//    <div>
+
+//    </div>
+//};
